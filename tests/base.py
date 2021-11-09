4eb260e7fefc0f58c585c6beefd13c33d8d72bd6
Ties <tg.klappe@gmail.com>
2021-11-09 10:22:02 +0100

    Adds tc8

--- tests/base.py
+++ tests/base.py
@@ -19,16 +19,11 @@ def login(username, password):
 
 
 """
-5.6.1   POST /_matrix/client/r0/register
+5.6.1   POST /_matrix/client/r0/register?kind=user
 
 Attempts to register a user and returns the access token and user id.
 """
-def register(username, password, is_guest=False):
-    kind = "user"
-
-    if is_guest:
-        kind = "guest"
-
+def register(username, password):
     #  _______________________
     # |       Stage 0         |
     # | No auth               |
@@ -37,7 +32,7 @@ def register(username, password, is_guest=False):
     # |_______________________|
 
     session = post_request(
-        "/_matrix/client/r0/register?kind=" + kind, {}).json()["session"]
+        "/_matrix/client/r0/register?kind=user", {}).json()["session"]
 
     body = {
         "auth": {
@@ -55,7 +50,28 @@ def register(username, password, is_guest=False):
     # | |_Request_1_________| |
     # |_______________________|
 
-    response = post_request("/_matrix/client/r0/register?kind=user", body).json()
+    response = post_request(
+        "/_matrix/client/r0/register?kind=user", body).json()
+
+    return (response["access_token"], response["user_id"])
+
+
+"""
+5.6.1   POST /_matrix/client/r0/register?kind=guest
+
+Attempts to register a guest user and returns the access token and user id.
+"""
+def register_guest(username, password):
+    body = {
+        "auth": {
+            "type": "m.login.dummy"
+        },
+        "username": username,
+        "password": password
+    }
+
+    response = post_request(
+        "/_matrix/client/r0/register?kind=guest", body).json()
 
     return (response["access_token"], response["user_id"])
 
@@ -83,9 +99,11 @@ def create_room(name, is_private, access_token):
 10.4.2.2   POST /_matrix/client/r0/rooms/{roomId}/join
 """
 def join_room(access_token, room_id):
-    response = post_request("/_matrix/client/r0/rooms/" + room_id + "/join", {}, access_token)
+    response = post_request("/_matrix/client/r0/rooms/" +
+                            room_id + "/join", {}, access_token)
 
     return response.status_code
+
 
 """
 Execute a POST request towards the local matrix server with an optional access token.
@@ -97,6 +115,20 @@ def post_request(endpoint, body, access_token=None):
         headers_dict["Authorization"] = "Bearer " + access_token
 
     return requests.post("http://localhost:8008" + endpoint,
+                         headers=headers_dict,
+                         json=body
+                         )
+
+"""
+Execute a PUT request towards the local matrix server with an optional access token.
+"""
+def put_request(endpoint, body, access_token=None):
+    headers_dict = dict()
+
+    if access_token:
+        headers_dict["Authorization"] = "Bearer " + access_token
+
+    return requests.put("http://localhost:8008" + endpoint,
                          headers=headers_dict,
                          json=body
                          )
