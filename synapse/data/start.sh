8dcdb01776c88ab0399bef698acb5cb6c6960ec8
Ruben Hensen <ruben.hensen@protonmail.com>
2021-11-09 16:52:22 +0100

    Add checks if files exist

--- synapse/data/start.sh
+++ synapse/data/start.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
-rm -rf /data/media_store
-rm -f /data/homeserver.db
+#!/bin/sh
+
+[ -d /data/media_store ] && rm -rf /data/media_store
+[ -f /data/homeserver.db ] && rm -f /data/homeserver.db
 python /start.py
