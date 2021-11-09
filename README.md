0d0a8121c485ceece27438cf32a6ab17133d3302
Ruben Hensen <ruben.hensen@protonmail.com>
2021-11-09 16:15:29 +0100

    Add results file

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# Prerequisites
+- Docker
+
 # Run tests
 docker compose up
+
+# Results
+Results can be found in the testing/results.out file
 
--- testing/runtests.sh
+++ testing/runtests.sh
@@ -12,8 +12,7 @@ python tests/new-user.py -u marnick -p marnickssecret123 --no-admin -c /synapsec
 
 echo "runtests.sh: Run tests without options"
 # Tests without options
-find tests-no-options -maxdepth 1 -type f -exec python {} \;
-# find /tests -maxdepth 1 -type f -exec python {} \; > results.out
+find tests-no-options -maxdepth 1 -type f -exec python {} \; > results.out
 
 echo "runtests.sh: Done"
 
