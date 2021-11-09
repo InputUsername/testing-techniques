c1194c913c4eb87c91fce8d3f5c270ae217c165e
Ruben Hensen <ruben.hensen@protonmail.com>
2021-11-09 16:32:25 +0100

    Bug fixes

--- testing/runtests.sh
+++ testing/runtests.sh
@@ -6,13 +6,9 @@ echo "runtests.sh: Wait for Synapse"
 # Wait untill synapse has fully loaded
 sleep 2
 
-echo "runtests.sh: Run tests with options"
-# Tests with options here
-python tests/new-user.py -u marnick -p marnickssecret123 --no-admin -c /synapseconfig/homeserver.yaml http://synapse:8008
-
 echo "runtests.sh: Run tests without options"
 # Tests without options
-find tests-no-options -maxdepth 1 -type f -exec python {} \; > results.out
+find tests -maxdepth 1 -type f -exec python {} \; > results.out
 
 echo "runtests.sh: Done"
 
