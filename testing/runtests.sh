f031b0c7ff2ada1e6c05892e2a1e8ef5652e2457
Ties <tg.klappe@gmail.com>
2021-11-10 10:33:50 +0100

    Disable synapse logging so we can observe assertion failures

--- testing/runtests.sh
+++ testing/runtests.sh
@@ -6,7 +6,7 @@ echo "runtests.sh: Wait for Synapse"
 # Wait untill synapse has fully loaded
 sleep 5
 
-echo "runtests.sh: Run tests without options"
+echo "runtests.sh: Running tests"
 # Tests without options
 find tests -maxdepth 1 -type f -exec python {} \; > results.out
 
