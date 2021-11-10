5e58bbc7a2392d253262bf6582a63f7e45d3aa48
Ruben Hensen <ruben.hensen@protonmail.com>
2021-11-10 21:40:29 +0100

    Add tc7

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ docker-compose up
 
 # Results
 Results can be found in the testing/results.out file
+
+# Done
+Stop the server by pressing CTRL + C or `docker-compose down`
--- testing/tests/tc3_invite_user_to_private_room.py
+++ testing/tests/tc3_invite_user_to_private_room.py
@@ -26,7 +26,7 @@ def main():
     # Execute the test case and assert the (expected) post condition
     assert(invite(access_token_1, user_id_2, room_id) == 200)
 
-    print("Test case 3 (invite user to private room) successfull")
+    print("Test case 3 (invite user to private room) successful")
 
 
 if __name__ == "__main__":
