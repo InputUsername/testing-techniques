7a0fe1cd776f2d6fdc8efd3a2bdc108c1c3b732d
Ties <tg.klappe@gmail.com>
2021-11-10 12:21:34 +0100

    Finish tc10

--- testing/tests/tc10_two_identical_addresses_at_same_time.py
+++ testing/tests/tc10_two_identical_addresses_at_same_time.py
@@ -2,7 +2,8 @@ from base import *
 
 import threading
 
-sync = threading.Lock()
+no_conflict = False
+conflict = False
 
 """
         PUT /_matrix/client/r0/directory/room/{roomAlias}
@@ -24,26 +25,29 @@ class IdenticalAddressTester(threading.Thread):
 
         self.access_token = access_token
         self.room_id = room_id
-        self.address = "%23identicalalias:synapse"
+        self.address = "%23identicalalias:my.matrix.host"
 
     def run(self):
-        if sync.locked():
-            assert(change_room_alias(self.access_token,
-                   self.room_id, self.address) == 400)
-        else:
-            sync.acquire()
-            assert(change_room_alias(self.access_token,
-                   self.room_id, self.address) == 200)
+        global no_conflict
+        global conflict
+
+        result = change_room_alias(self.access_token, self.room_id, self.address)
+
+        if result == 200:
+            no_conflict = True
+
+        if result == 409:
+            conflict = True
 
 
 def main():
     # [Precondition]: create test users 1 and 2
-    (access_token_1, _user_id_1) = register("tc10_user", "privatepassword1A")
-    (access_token_2, _user_id_2) = register("tc10_user", "privatepassword1A")
+    (access_token_1, _user_id_1) = register("tc10_user_1", "privatepassword1A")
+    (access_token_2, _user_id_2) = register("tc10_user_2", "privatepassword1A")
 
     # [Precondition]: create public test room
-    room_id_1 = create_room("tc10_public_room", False, access_token_1)
-    room_id_2 = create_room("tc10_public_room", False, access_token_2)
+    room_id_1 = create_room("tc10_public_room_1", False, access_token_1)
+    room_id_2 = create_room("tc10_public_room_2", False, access_token_2)
 
     threads = []
 
@@ -59,6 +63,8 @@ def main():
 
     for t in threads:
         t.join()
+
+    assert (conflict and no_conflict)
 
     print("Test case 10 (two identical addresses at the same time) successfull")
 
