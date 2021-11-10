83094e68d9f58b98c0cc8cdcbc4607dd03b29c66
Ruben Hensen <ruben.hensen@protonmail.com>
2021-11-10 19:03:45 +0100

    Add stderr to output file

--- testing/runtests.sh
+++ testing/runtests.sh
@@ -8,7 +8,7 @@ sleep 5
 
 echo "runtests.sh: Running tests"
 # Tests without options
-find tests -maxdepth 1 -type f -exec python {} \; > results.out
+find tests -maxdepth 1 -type f -exec python {} \; > results.out 2>&1
 
 echo "runtests.sh: Done"
 
