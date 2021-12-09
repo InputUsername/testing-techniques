3ab30de71aa5f846290e5ffd1b125b94bb0800f6
Ties <tg.klappe@gmail.com>
2021-12-09 10:06:31 +0100

    tried some regexes.

--- adapter/adapter.py
+++ adapter/adapter.py
@@ -1,5 +1,9 @@
 import socket
+import re
+
 from enum import Enum
+from base import *
+
 
 """
 Matrix requests types supported by this adapter.
@@ -7,15 +11,18 @@ Matrix requests types supported by this adapter.
 Following the endpoints as described at https://matrix.org/docs/spec/client_server/r0.6.1
 """
 class MatrixRequests(Enum):
+    # No official Matrix command. This uses the interactive authentication API
+    # to create an access token for a user.
+    AUTHORIZE = 'Authorize'
+
     # 5.6.1   POST /_matrix/client/r0/register?kind=user
-    REGISTER = 'register'
+    REGISTER = 'Register'
 
     # 5.5.2   POST /_matrix/client/r0/login
-    LOGIN = 'login'
+    LOGIN = 'Login'
 
     # 10.1.1   POST /_matrix/client/r0/createRoom
-    CREATE_ROOM = 'create_room'
-
+    CREATE_ROOM = 'CreateRoom'
 
 """
 Process a TorXakis request into a Matrix request.
@@ -23,15 +30,27 @@ Process a TorXakis request into a Matrix request.
 [<request_type> <authentication_type> <optional_authentication_token> <request_body_arguments>]
 """
 def process(message: str, sockfile):
-    segments = message.split()
-    if len(segments) <= 0:
-        print('Received empty message')
-    else:
-        request_identifier = segments[0]
+    # The command, followed by an opening paranthesis, followed by any number of arguments, 
+    # follow by a closing paranthesis.
+    pattern = '(.+)\((.+)*\)'
+    match = re.search(pattern, message)
+
+    if match:
+        command = match.group(1)
+        arguments = match.group(2)
+        arguments = re.sub('["]', '', arguments)
+
+        argument_list = arguments.split(',')
+
+        print(f'cmd: {command}, args: {argument_list}')
+
         try:
-            request = MatrixRequests(request_identifier)
+            request = MatrixRequests(command)
 
             match request:
+                case MatrixRequests.AUTHORIZE:
+                    # token = acquire_session_token()
+                    sockfile.write(f'Bearer("token")')
                 case MatrixRequests.REGISTER:
                     print("REGISTER")
                     sockfile.write("200\n")
@@ -40,9 +59,9 @@ def process(message: str, sockfile):
                     print("LOGIN")
                 case MatrixRequests.CREATE_ROOM:
                     print("CREATE ROOM")
-        except ValueError as e:
-            print(f"Unknown request '{request_identifier}'")
 
+        except ValueError as e:
+            print(f"Unknown command '{command}'")
 
 def main():
     with socket.create_server(('', 7890)) as server:
@@ -56,7 +75,6 @@ def main():
                 break
 
             process(message, sockfile)
-
 
 if __name__ == '__main__':
     main()
