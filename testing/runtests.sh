d3c8588e8de35771c6e8fcd79eb3f42395ce55e7
Ruben Hensen <ruben.hensen@protonmail.com>
2021-12-14 22:58:58 +0100

    Add torxakis including purpose

--- testing/runtests.sh
+++ testing/runtests.sh
@@ -5,10 +5,29 @@ python3 ./adapter/adapter.py 7890 &
 echo "Start second adapter"
 python3 ./adapter/adapter.py 7891 &
 
-# echo "tester Matrix Sut
-# test 40
-# exit" | ./torxakis-0.9.0.x86_64.AppImage --appimage-extract-and-run ./model/Matrix.txs
-
+echo "
+****************************************
+Start Torxakis test 100
+****************************************
+"
 echo "tester Matrix Sut
-test 40
+test 100
 exit" | ./torxakis-0.9.0.x86_64.AppImage --appimage-extract-and-run ./model/Matrix.txs
+
+echo "Kill old adapters"
+kill $(jobs -p)
+
+echo "Start first adapter"
+python3 ./adapter/adapter.py 7890 &
+
+echo "Start second adapter"
+python3 ./adapter/adapter.py 7891 &
+
+echo "
+****************************************
+Start Torxakis test 100 w/ purpose
+****************************************
+"
+echo "tester Matrix Sut
+test 100
+exit" | ./torxakis-0.9.0.x86_64.AppImage --appimage-extract-and-run ./model/Matrix.txs ./model/MatrixPurp.txs
