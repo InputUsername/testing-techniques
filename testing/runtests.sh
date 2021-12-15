12e00b1e2fd6801ffed9ad478935610e3da23699
Ruben Hensen <ruben.hensen@pm.me>
2021-12-15 15:00:28 +0100

    Change test order and altwalker test command

--- testing/runtests.sh
+++ testing/runtests.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-cd altwalker-tests
-# ls -la
-# altwalker --version
-gw --version
-altwalker online tests -m models/matrix.json "quick_random(vertex_coverage(100) && edge_coverage(100))"
-cd ..
-
 echo "Start first adapter"
 python3 ./adapter/adapter.py 7890 &
 
@@ -38,3 +31,12 @@ Start Torxakis test 100 w/ purpose
 echo "tester Matrix PurpMessageRedaction Sut
 test 100
 exit" | ./torxakis-0.9.0.x86_64.AppImage --appimage-extract-and-run ./model/Matrix.txs ./model/MatrixPurp.txs
+
+echo "
+****************************************
+Start Altwalker test
+****************************************
+"
+cd altwalker-tests
+altwalker online tests -m models/matrix.json "random(edge_coverage(100) || time_duration(500))"
+cd ..
